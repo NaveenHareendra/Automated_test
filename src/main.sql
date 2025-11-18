64db270cf6f8613cacdf7129c5f709e39b60accd
Naveen Warnakulasuriya <=naveen.hareendra@gmail.com>
2025-11-18 15:04:17 +0530

    changing into a SQL

---
+++ src/main.sql
@@ -0,0 +1,27 @@
+
+Create table babablacksheep();
+
+
+/*
+# from openai import OpenAI
+# import requests
+# client = OpenAI(
+#   api_key=""
+# )
+
+
+# # //_git/
+# # r = requests.get('https://dev.azure.com/PeoplesHR/HRM/_apis/git/repositories/HRM-DB/pullrequests?api-version=7.1')
+
+# # print(r)
+# what is this?? not a variable? Error
+
+# response = client.responses.create(
+#   model="o4-mini",
+#   reasoning={"effort": "low"},
+#   input="are you aware of good dotnet coding practices",
+#   store=True,
+# )
+
+# print(response.output_text);
+*/
