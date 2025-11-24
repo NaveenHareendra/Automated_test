1d210e6391369e0b0cf618e8d724a35d4f397d07
Naveen Warnakulasuriya <=naveen.hareendra@gmail.com>
2025-11-24 11:16:29 +0530

    Changed the folder name

--- src/Students/anSQLfile.sql
+++ src/Students/anSQLfile.sql
@@ -1,6 +1,7 @@
 
 --As this SQL file should be reviewed by AI agent, it should properly identify below object name and its relevant folder names are equal or not.
 --example if folder name is dothis, table should create table dothis ( ... )
+--Change the folder name...
 
 create table Employees (
     EmployeeID int primary key,
