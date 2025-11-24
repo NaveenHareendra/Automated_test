10afe01dc597b9693b0ce0c4ebd0d83efd062ddd
Naveen Warnakulasuriya <=naveen.hareendra@gmail.com>
2025-11-24 11:42:21 +0530

    Fixing AI reviewer's comments.

--- src/Employees/anSQLfile.sql
+++ src/Employees/anSQLfile.sql
@@ -4,7 +4,9 @@ This commit is only for SQLs... Deleted pythons AI check below...
 */
 --As this SQL file should be reviewed, it should properly identify below object name and its relevant folder names are equal or not.
 --For example if folder name is Employees, table must be Employees too
-create table Emp__1 (
+--Reviewer should comment if the folder name and the object name is equal now .... 
+
+create table Employees (
     EmployeeID int primary key,
     FirstName varchar(50),
     LastName varchar(50),
