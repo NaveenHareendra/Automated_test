c81f5a6d30c04bc90d6f7ec9aaebac33f3cc0a68
Naveen Warnakulasuriya <=naveen.hareendra@gmail.com>
2025-11-24 10:59:49 +0530

    Qodo AI please review this

---
+++ src/test.py
@@ -0,0 +1,9 @@
+#this is a bad python file , should be gone into a separate folder too .
+#This code has no structure
+
+
+blah =10;
+
+for i in range(pow((blah),2)):
+    for j in range(pow((blah),2)):
+        print("Hello World ", j)
