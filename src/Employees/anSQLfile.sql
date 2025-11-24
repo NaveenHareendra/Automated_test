e8ac52a3f6011a98f3603cc60105ebd64d4635eb
Naveen Warnakulasuriya <=naveen.hareendra@gmail.com>
2025-11-24 11:38:41 +0530

    commit changes for SQL

--- src/Employees/anSQLfile.sql
+++ src/Employees/anSQLfile.sql
@@ -2,11 +2,9 @@
 /*
 This commit is only for SQLs... Deleted pythons AI check below...
 */
---As this SQL file should be reviewed by AI agent, it should properly identify below object name and its relevant folder names are equal or not.
---example if folder name is dothis, table should create table dothis ( ... )
-
-
-create table Employees (
+--As this SQL file should be reviewed, it should properly identify below object name and its relevant folder names are equal or not.
+--For example if folder name is Employees, table must be Employees too
+create table Emp__1 (
     EmployeeID int primary key,
     FirstName varchar(50),
     LastName varchar(50),
